9a6771305f91f6ba122323137054d60a5221a189
Jan Krüger <jk@jk.gs>
2011-12-13 02:38:32 +0100

    plugins/voice: new command for users to voice themselves (IRC/ctl)

---
+++ plugins/voice.pm
@@ -0,0 +1,26 @@
+use POE;
+{
+	ctl_commands => {
+		voice => sub {
+			my ($client, $data, @args) = @_;
+			&BotCtl::require_control or return;
+			if (!$BotIrc::irc->is_channel_member($BotIrc::config->{channel}, $args[0])) {
+				$client->put("error:notinchan:That user couldn't be found in the channel.");
+			} else {
+				$BotIrc::irc->yield(mode => $BotIrc::config->{channel} => "+v" => $args[0]);
+			}
+		},
+	},
+	irc_commands => {
+		voice => sub {
+			my ($source, $targets, $args, $auth) = @_;
+			my $rpath = &BotIrc::return_path // return 0;
+			my $nick = BotIrc::nickonly($source);
+
+			$BotIrc::irc->yield(mode => $BotIrc::config->{channel} => "+v" => $source);
+			return 1;
+		},
+	},
+};
+
+
