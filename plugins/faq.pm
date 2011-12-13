f1e81183064495f62917493b3823014587301c14
Jan Krüger <jk@jk.gs>
2011-12-13 18:29:46 +0100

    plugins/faq: add control command faq_list

--- plugins/faq.pm
+++ plugins/faq.pm
@@ -2,9 +2,29 @@ use POE;
 {
 	on_load => sub {
 		$BotIrc::heap->{faq_cache} = undef;
+		$BotIrc::heap->{faq_cacheupdate} = sub {
+			return 1 if defined $BotIrc::heap->{faq_cache};
+			my $error = shift || sub {};
+			my $faq = BotIrc::read_file($BotIrc::config->{faq_cachefile}) or do {
+				BotIrc::error("FAQ cache broken: $!");
+				$error->("FAQ cache is broken. The bot owner has been notified.");
+				return 0;
+			};
+			while ($faq =~ /<span id="([a-z-]+)" title="(.*?)">/g) {
+				$BotIrc::heap->{faq_cache}{$1} = $2;
+			}
+			return 1;
+		};
 	},
 	before_unload => sub {
 		delete $BotIrc::heap->{faq_cache};
+	},
+	control_commands => {
+		faq_list => sub {
+			my ($client, $data, @args) = @_;
+			$BotIrc::heap->{faq_cacheupdate}(sub { send($client, "error", "faqcache_broken", $_); }) or return;
+			BotCtl::send($client, "ok", to_json($BotIrc::heap->{faq_cache}, {utf8 => 1, canonical => 1}));
+		},
 	},
 	irc_commands => {
 		faq_update => sub {
@@ -24,16 +44,7 @@ use POE;
 		my $rpath = &BotIrc::return_path(@_[ARG0, ARG1]) // return 0;
 		my $nick = BotIrc::nickonly($_[ARG0]);
 
-		if (!defined $BotIrc::heap->{faq_cache}) {
-			my $faq = BotIrc::read_file($BotIrc::config->{faq_cachefile}) or do {
-				BotIrc::error("FAQ cache broken: $!");
-				$BotIrc::irc->yield(privmsg => $rpath => "$nick: FAQ cache is broken. The bot owner has been notified.");
-				return 1;
-			};
-			while ($faq =~ /<span id="([a-z-]+)" title="(.*?)">/g) {
-				$BotIrc::heap->{faq_cache}{$1} = $2;
-			}
-		}
+		$BotIrc::heap->{faq_cacheupdate}(sub { $BotIrc::irc->yield(privmsg => $rpath => "$nick: ".$_); }) or return 1;
 		return 1 if (!exists $BotIrc::heap->{faq_cache}{$page});
 		my $info = $BotIrc::heap->{faq_cache}{$page};
 		if ($info) {
