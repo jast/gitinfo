de04ab2020b33f57e750caea2138d5dd84b6e74f
Jan Krüger <jk@jk.gs>
2012-02-13 21:25:07 +0100

    plugins/voice: introduce missing configuration for target channel
    When multi-channel support was added, the voice plugin broke because it
    didn't think to use its own config to determine which channel to voice
    people in. Introduce that, a bit belatedly.
    
    Signed-off-by: Jan Krüger <jk@jk.gs>

--- plugins/voice.pm
+++ plugins/voice.pm
@@ -4,10 +4,10 @@ use POE;
 		voice => sub {
 			my ($client, $data, @args) = @_;
 			&BotCtl::require_control or return;
-			if (!$BotIrc::irc->is_channel_member($BotIrc::config->{channel}, $args[0])) {
+			if (!$BotIrc::irc->is_channel_member($BotIrc::config->{voice_channel}, $args[0])) {
 				$client->put("error:notinchan:That user couldn't be found in the channel.");
 			} else {
-				$BotIrc::irc->yield(mode => $BotIrc::config->{channel} => "+v" => $args[0]);
+				$BotIrc::irc->yield(mode => $BotIrc::config->{voice_channel} => "+v" => $args[0]);
 			}
 		},
 	},
@@ -17,7 +17,11 @@ use POE;
 			BotIrc::check_ctx() or return 1;
 			my $nick = BotIrc::nickonly($source);
 
-			$BotIrc::irc->yield(mode => $BotIrc::config->{channel} => "+v" => $source);
+			if (!$BotIrc::irc->is_channel_member($BotIrc::config->{voice_channel}, $nick)) {
+				BotIrc::send_noise("What? You're not even in $BotIrc::config->{voice_channel}!");
+				return 1;
+			}
+			$BotIrc::irc->yield(mode => $BotIrc::config->{voice_channel} => "+v" => $source);
 			return 1;
 		},
 	},
