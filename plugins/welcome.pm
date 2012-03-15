b2d84598b7e9c92f83141b3eeaae9cbbc2698bb2
Jan Krüger <jk@jk.gs>
2012-03-15 13:34:08 +0100

    plugins/welcome: new plugin to automatically welcome users who say hi
    Signed-off-by: Jan Krüger <jk@jk.gs>

---
+++ plugins/welcome.pm
@@ -0,0 +1,21 @@
+use POE;
+my %joined_users = ();
+{
+	irc_on_join => sub {
+		$joined_users{BotIrc::nickonly(lc $_[ARG0])} = 1;
+	},
+	irc_on_public => sub {
+		BotIrc::check_ctx() or return 1;
+		my $source = BotIrc::ctx_source();
+		my $chan = BotIrc::ctx_target('wisdom');
+		return 0 if !exists $joined_users{lc $source};
+		delete $joined_users{lc $source};
+		return 0 if $_[ARG2] !~ /^(?:hi|hello)\b/i;
+		my $msg;
+		return 0 if !defined($msg = $BotIrc::config->{welcome_channels}{lc $chan});
+
+		BotIrc::ctx_set_addressee($source);
+		BotIrc::send_wisdom($msg);
+		return 1;
+	},
+};
