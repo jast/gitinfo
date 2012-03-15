1eb8e30f22ba4f5d9d5760fa4cae30db2b134b11
Jan Krüger <jk@jk.gs>
2012-03-15 20:10:28 +0100

    plugins/duckduckgo: new utility commands for stuff and the web
    Introduce two new commands:
    
    - .info: uses DDG's zero-click info API to explain/solve/find pretty
      much any thing there is (except standard web search). For instance, it
      can find Github repositories, define terms, perform calculations, look
      up the weather, etc.
    - .search: uses DDG's web search and returns the top three results.
    
    Signed-off-by: Jan Krüger <jk@jk.gs>

---
+++ plugins/duckduckgo.pm
@@ -0,0 +1,102 @@
+use POE;
+use HTML::HTML5::Parser;
+use HTML::Entities;
+use HTTP::Request::Common;
+use JSON;
+use URI::Escape;
+use XML::LibXML::XPathContext;
+
+my $dolink = sub { BotPlugin::call('templink', 'make', shift); };
+{
+	dependencies => [ 'templink' ],
+	irc_commands => {
+		info	=> sub {
+			my ($source, $targets, $args, $auth) = @_;
+			BotIrc::check_ctx() or return;
+			my $nick = BotIrc::nickonly($source);
+			my $ctx = BotIrc::ctx_frozen;
+
+			BotHttp::get("http://duckduckgo.com/?q=".uri_escape($args)."&format=json&no_redirect=1&no_html=1&skip_disambig=1", sub {
+				my $data = eval { decode_json(shift); };
+				if ($@) {
+					BotIrc::send_noise($ctx, ".info error: parsing JSON: $@");
+					return;
+				}
+				# Most interested in: related topics
+				if ($data->{Type} =~ /^[CD]$/o) {
+					my @topics = @{$data->{RelatedTopics}};
+					my $suffix = (@topics > 3) ? " | ..." : "";
+					splice @topics, 3;
+					my $topics = join(" | ", map { "$_->{Text} <". $dolink->($_->{FirstURL}) .">" } @topics);
+					BotIrc::send_wisdom($ctx, "$topics$suffix");
+					return;
+				}
+				# Bang command
+				if ($data->{Redirect}) {
+					BotIrc::send_wisdom($ctx, $dolink->($data->{Redirect}));
+					return;
+				}
+				# Answer from calculator etc.
+				if ($data->{Answer} && $data->{AnswerType}) {
+					BotIrc::send_wisdom($ctx, "[$data->{AnswerType}] $data->{Answer}");
+					return;
+				}
+				# Abstract
+				if ($data->{AbstractText}) {
+					BotIrc::send_wisdom($ctx, "$data->{Heading}: $data->{AbstractText} <$data->{AbstractURL}> [from $data->{AbstractSource}]");
+					return;
+				}
+				# Definition
+				if ($data->{Definition}) {
+					BotIrc::send_wisdom($ctx, "$data->{Definition} <$data->{DefinitionURL}> [from $data->{DefinitionSource}]");
+					return;
+				}
+				BotIrc::send_wisdom($ctx, ".info: nothing found.");
+			}, sub {
+				BotIrc::send_noise($ctx, ".info error: query '$args' failed: ".shift);
+				return;
+			});
+		},
+		search	=> sub {
+			my ($source, $targets, $args, $auth) = @_;
+			BotIrc::check_ctx() or return;
+			my $nick = BotIrc::nickonly($source);
+			my $req = POST('http://duckduckgo.com/html', [q => $args]);
+			my $ctx = BotIrc::ctx_frozen;
+
+			BotHttp::request($req, sub {
+				my $dom = eval { HTML::HTML5::Parser->new->parse_string(shift); };
+				if ($@) {
+					BotIrc::send_noise($ctx, ".search error: parsing HTML: $@");
+					return;
+				}
+				my $xpc = XML::LibXML::XPathContext->new($dom);
+				$xpc->registerNs('x', 'http://www.w3.org/1999/xhtml');
+				my @nodes = $xpc->findnodes('descendant::x:div[@class="links_main links_deep"]');
+				if (!@nodes) {
+					BotIrc::send_wisdom($ctx, ".search: nothing found.");
+					return;
+				}
+				my $suffix = (@nodes > 3) ? " | ..." : "";
+				splice @nodes, 3;
+				@nodes = map {
+					my $url = $xpc->findvalue('x:a[1]/@href', $_);
+					my $title = $xpc->findvalue("x:a[1]", $_);
+					$title =~ s/<(?:[^>]+)>//g;
+					$title = decode_entities($title);
+					$url = $dolink->($url) if $url;
+					$url ? "$title <$url>" : undef;
+				} @nodes;
+				if (!$nodes[0]) {
+					BotIrc::send_wisdom($ctx, ".search: nothing found.");
+					return;
+				}
+				my $nodes = join(" | ", @nodes);
+				BotIrc::send_wisdom($ctx, "$nodes$suffix");
+			}, sub {
+				BotIrc::send_noise($ctx, ".search error: query '$args' failed: ".shift);
+				return;
+			});
+		},
+	},
+};
