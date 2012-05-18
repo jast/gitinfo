057f71e45ec681c6d5716d82dccc4de00d751f8c
Jan Krüger <jk@jk.gs>
2012-05-18 19:26:54 +0200

    plugins/welcome: persist welcome flag
    Previously, any joining user would get welcomed if the first thing they
    said was something like "hi". This is suboptimal for returning visitors,
    so only do this the first time we ever see them.
    
    Signed-off-by: Jan Krüger <jk@jk.gs>

--- plugins/welcome.pm
+++ plugins/welcome.pm
@@ -1,23 +1,41 @@
 use POE;
-my %joined_users = ();
+my %welcomed_users = ();
 {
-	irc_on_join => sub {
-		$joined_users{BotIrc::nickonly(lc $_[ARG0])} = 1;
+	schemata => {
+		0 => [
+			"CREATE TABLE welcomed (
+				nick TEXT PRIMARY KEY NOT NULL,
+				channel TEXT NOT NULL)",
+		],
+	},
+	on_load => sub {
+		my $res = $BotDb::db->selectall_arrayref("SELECT nick, channel FROM welcomed", {Slice => {}});
+		for (@$res) {
+			$welcomed_users{$_->{channel}} //= {};
+			$welcomed_users{$_->{channel}}{$_->{nick}} = 1;
+		}
 	},
 	irc_on_public => sub {
 		BotIrc::check_ctx() or return 1;
 		my $source = BotIrc::ctx_source();
 		my $chan = BotIrc::ctx_target('wisdom');
-		return 0 if !exists $joined_users{lc $source};
-		delete $joined_users{lc $source};
-		return 0 if $_[ARG2] !~ /^(?:hi|hello)\b/i;
-		# arbitrary threshold; prevent responding to an actual question
-		return 0 if length($_[ARG2]) > 15;
+		return 0 if defined $welcomed_users{lc $chan}{lc $source};
+		# We'll want to check this first; no need to flag someone if
+		# the channel doesn't have a welcome message in the first place
 		my $msg;
 		return 0 if !defined($msg = $BotIrc::config->{welcome_channels}{lc $chan});
 
+		# No hi first thing? Put them on the naughty list
+		goto flag_as_welcomed if $_[ARG2] !~ /^(?:hi|hello|hey)\b/i;
+		# arbitrary threshold; prevent responding to an actual question
+		goto flag_as_welcomed if length($_[ARG2]) > 15;
+
 		BotIrc::ctx_set_addressee($source);
 		BotIrc::send_wisdom($msg);
-		return 1;
+	    flag_as_welcomed:
+		$BotDb::db->do("INSERT INTO welcomed (nick, channel) VALUES(?,?)", {}, lc $source, lc $chan);
+		$welcomed_users{lc $chan} //= {};
+		$welcomed_users{lc $chan}{lc $source} = 1;
+		return 0;
 	},
 };
