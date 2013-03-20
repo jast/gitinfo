5d80b688ce92ba466f0d6f857a2314bf5731c78e
Jan Krüger <jk@jk.gs>
2013-03-20 23:43:09 +0100

    plugins/say: support .action, too
    For certified CTCP ACTION goodness.

--- plugins/say.pm
+++ plugins/say.pm
@@ -1,6 +1,17 @@
 use POE;
 {
 	irc_commands => {
+		action => sub {
+			my ($source, $targets, $args, $auth) = @_;
+			my ($chan, $text) = split(/\s+/, $args, 2);
+			BotIrc::check_ctx(authed => 1, priv => 'say') or return;
+
+			BotIrc::ctx_redirect_to_channel('wisdom', $chan) or do {
+				BotIrc::send_noise("Channel '$chan' not found in config; denied.");
+				return;
+			};
+			BotIrc::send_wisdom("\001ACTION $text\001");
+		},
 		say => sub {
 			my ($source, $targets, $args, $auth) = @_;
 			my ($chan, $text) = split(/\s+/, $args, 2);
@@ -11,6 +22,6 @@ use POE;
 				return;
 			};
 			BotIrc::send_wisdom($text);
-		}
+		},
 	},
 };
