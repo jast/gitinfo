f4d527123395eaf9e915c418bffa426bbc8bd02e
Jan Krüger <jk@jk.gs>
2015-04-10 23:37:35 +0200

    plugins/git: new one to generate links to web viewers for Git repos
    Signed-off-by: Jan Krüger <jk@jk.gs>

---
+++ plugins/git.pm
@@ -0,0 +1,68 @@
+use Encode;
+use JSON;
+use POE;
+
+my $dolink = sub { BotPlugin::call('templink', 'make', shift); };
+my %repo_providers = (
+	github => {
+		shortlog => "https://github.com/{key}/commits/{ref}",
+		blob => "https://github.com/{key}/blob/{ref}/{path}",
+		tree => "https://github.com/{key}/tree/{ref}/{path}",
+	},
+	gitlab => {
+		shortlog => "https://gitlab.com/{key}/commits/{ref}",
+		blob => "https://gitlab.com/{key}/blob/{ref}/{path}",
+		tree => "https://gitlab.com/{key}/tree/{ref}/{path}",
+	},
+	bitbucket => {
+		shortlog => "https://bitbucket.org/{key}/commits/{ref}",
+		blob => "https://bitbucket.org/{key}/src/{ref}/{path}",
+		tree => "https://bitbucket.org/{key}/src/{ref}/{path}",
+	},
+	kernel => {
+		shortlog => "https://git.kernel.org/cgit/{key}/log/?h={ref}",
+		blob => "https://git.kernel.org/cgit/{key}/tree/{path}?h={ref}",
+		tree => "https://git.kernel.org/cgit/{key}/tree/{path}?h={ref}",
+	},
+	repo => {
+		shortlog => "http://repo.or.cz/w/{key}/shortlog/{ref}",
+		blob => "http://repo.or.cz/w/{key}/blob/{ref}:/{path}",
+		tree => "http://repo.or.cz/w/{key}/tree/{ref}:/{path}",
+	},
+);
+
+{
+	irc_on_anymsg => sub {
+		BotIrc::check_ctx(wisdom_auto_redirect => 1) or return 1;
+
+		while ($_[ARG2] =~ /git:(\S+)/ig) {
+			my $query = $1;
+			$query =~ s/^"(.+)"$/$1/;
+			my ($type, $key, $spec) = split(/:/, $query, 3);
+			next unless exists $repo_providers{$type};
+
+			my $prov = $repo_providers{$type};
+			my $ref = 'HEAD';
+			my $path;
+			if ($spec =~ s/^([^:]*)://) {
+				$ref = $1 unless $1 eq '';
+			} else {
+				$ref = $spec;
+				$path = []; # sticky undef
+			}
+			$path = $spec unless ref $path;
+			undef $path if ref $path;
+
+			my $mode = defined $path ? 'blob' : 'shortlog';
+			$mode = 'tree' if $path && $path =~ m#/$#;
+
+			my $url = $repo_providers{$type}{$mode};
+			$url =~ s/\{key\}/$key/;
+			$url =~ s/\{ref\}/$ref/;
+			$url =~ s/\{path\}/$path/;
+
+			BotIrc::send_wisdom("Git web link: $url");
+		}
+		return 0;
+	},
+};
